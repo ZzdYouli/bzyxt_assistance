c0097f60f614650e2b94850b3e608bc03b6b6142
ZzdYouli <2052549604@qq.com>
2025-04-04 14:26:48 +0800

    躺床辅助器

---
+++ bzyxt/arts_switch.py
@@ -0,0 +1,11 @@
+def arts_switch(art_name):
+    if art_name == "玉女剑法":
+        return "yuNvJian"
+
+    elif art_name == "飞蝶舞步":
+        return "feiDie"
+
+    elif art_name == "素心掌":
+        return "suXinZhang"
+
+    return art_name
