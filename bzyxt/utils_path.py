58372defd78cb528bf348a9ebb3d0221d6819065
ZzdYouli <2052549604@qq.com>
2025-04-10 22:29:02 +0800

    兼容性改善

---
+++ bzyxt/utils_path.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+def get_adb_path():
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, "tools", "adb.exe")
+
+
+def get_tesseract_path():
+    """
+    自动识别 tesseract.exe 路径，兼容 PyInstaller 打包环境
+    """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, "tesseract-OCR", "tesseract.exe")
